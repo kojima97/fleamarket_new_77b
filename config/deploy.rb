f2338018b1cb65558a523dbfd292c930882712d0
kojimachiaki <why_chisai@yahoo.co.jp>
2020-06-18 14:54:41 +0900

    Update deploy.rb
    master.key => secrets upload edit

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,19 +25,31 @@ namespace :deploy do
     invoke 'unicorn:stop'
     invoke 'unicorn:start'
   end
-
-  desc 'upload master.key'
+  
+  desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/master.key', "#{shared_path}/config/master.key")
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
   end
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+#   desc 'upload master.key'
+#   task :upload do
+#     on roles(:app) do |host|
+#       if test "[ ! -d #{shared_path}/config ]"
+#         execute "mkdir -p #{shared_path}/config"
+#       end
+#       upload!('config/master.key', "#{shared_path}/config/master.key")
+#     end
+#   end
+#   before :starting, 'deploy:upload'
+#   after :finishing, 'deploy:cleanup'
+# end
 
 
 # Default branch is :master
