c314b3055d95f19536629356a38abcade0ee722e
kouheimochida <65533949+kouheimochida@users.noreply.github.com>
2020-06-22 18:52:01 +0900

    sign-up-commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, password_length: 7..128
 
 
-  # validates :nickname,:first_name,:first_name_furigana,:last_name,:last_name_furigana,:birthday,:email,  presence: true
-  # validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  # validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  # validates :email, uniqueness: true
-  # validates :email, format: { with:/\A\S+@\S+\.\S+\z/ }
+  validates :nickname,:first_name,:first_name_furigana,:last_name,:last_name_furigana,:birthday,:email,  presence: true
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :email, uniqueness: true
+  validates :email, format: { with:/\A\S+@\S+\.\S+\z/ }
  
   has_one :address
 end
