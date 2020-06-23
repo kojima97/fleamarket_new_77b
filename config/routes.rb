96c45a2dcfe7146e4414c923a1c49886196a17ce
kojimachiaki <why_chisai@yahoo.co.jp>
2020-06-23 12:24:44 +0900

    product-purchase-confirmation
    #what
    購入内容確認ページ
    #why
    購入を確定するページに当たるため。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
-  resources :products, only: [:index, :new]
+  resources :products, only: [:index, :new] do
+    collection do
+      get 'purchase_details_confirmation'
+    end
+  end
 end
