3e5678ae5bb7645efd7e53740501b350c9ba7a3d
uemura-39 <k.u19970228@gmail.com>
2020-06-23 21:18:05 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'cards/new'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -10,4 +12,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
   resources :products, only: [:index, :new]
+  resources :users, only: [:show, :destroy]
+  resources :cards, only: [:new]
 end
