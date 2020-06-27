453d529ed2e0f67274af2f34f3cc82fecb6db7e1
kojimachiaki <why_chisai@yahoo.co.jp>
2020-06-27 21:45:14 +0900

    カテゴリー
    仮実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,8 @@ class ProductsController < ApplicationController
     if user_signed_in?
       @product = Product.new
       @product.product_photos.build
+      @category_parent_array = Category.where(ancestry: nil).pluck(:name)
+      @category_parent_array.unshift("---")
     else
       redirect_to root_path, notice: 'ログインもしくはサインインしてください。'
     end
