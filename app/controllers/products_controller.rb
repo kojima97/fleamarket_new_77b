c22a015b1e3ea08c000fed2826ee8d379e152e3a
kojimachiaki <why_chisai@yahoo.co.jp>
2020-06-27 17:17:30 +0900

    商品名・商品出品
    フォーム変更
    カラム確認
    制約null: false

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,12 @@ class ProductsController < ApplicationController
   end
 
   def new
+    if user_signed_in?
+      @product = Product.new
+      @product.product_photos.build
+    else
+      redirect_to root_path, notice: 'ログインもしくはサインインしてください。'
+    end
   end
 
   def purchase_details_confirmation
