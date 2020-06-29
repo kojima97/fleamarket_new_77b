e53e53d4c7b1b7adc49ef5a9bae89d2406eb0dd2
YourName <you@example.com>
2020-06-29 19:27:07 +0900

    pick-up-list

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :product_photos, dependent: :destory
+  has_many :product_photos
 
   enum status: { 新品・未使用: 0, 未使用に近い: 1, 目立った傷や汚れなし: 2, やや傷や汚れあり: 3, 傷や汚れあり: 4, 全体的に状態が悪い: 5 }
   enum bear: { 送料込み（出品者負担）: 0, 着払い（購入者負担）: 1}
