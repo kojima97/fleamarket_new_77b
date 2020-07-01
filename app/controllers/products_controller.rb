02bce609c0c7b6a9bf438efd2fde91598cd8aeb9
uemura-39 <k.u19970228@gmail.com>
2020-07-01 16:19:11 +0900

    カテゴリー機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,49 @@
 class ProductsController < ApplicationController
-
-  before_action :set_parents, only: [:new, :create]
-
-  def index
+  
+  def index  
   end
 
   def show
   end
 
   def new
-    @product = Product.new
-    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
-    @category_parent_array.unshift("---")
+      @product = Product.new
+      @product.product_photos.new
+      @category_parent_array = ["---"]
+      Category.where(ancestry: nil).each do |parent|
+        @category_parent_array << parent.name
+      end
   end
 
   def create
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   def purchase_details_confirmation
   end
 
-  def set_parents
-    @parents = Category.where(ancestry: nil)
+  def get_category_children
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :explanation, :category_id, :status, :bear, :area, :brand, :days, :price, product_photos_attributes: [:photo, :_destroy, :id]).merge(exhibitor_user_id: current_user.id)
+  end
+
 
 end
 
