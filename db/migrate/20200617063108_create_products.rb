ce5c237055145d9397dd528410f11d7e397aad12
kojimachiaki <why_chisai@yahoo.co.jp>
2020-07-03 21:28:02 +0900

    出品ページ修正
    出品完了

--- db/migrate/20200617063108_create_products.rb
+++ db/migrate/20200617063108_create_products.rb
@@ -1,7 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[5.0]
   def change
     create_table :products do |t|
-      t.integer :product_id, null: false
       t.string :name, null: false
       t.integer :price, null: false
       t.string :status, null: false
