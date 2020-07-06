e9de83df68a1caf25f675ba513eb3d231a6d3349
kouheimochida <65533949+kouheimochida@users.noreply.github.com>
2020-07-06 13:24:30 +0900

    payjp

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,17 +1,17 @@
 class PurchasesController < ApplicationController
-  
   require "payjp"
 
   def buy
     # 購入する商品を引っ張ってきます。
     @product = Product.find(params[:product_id])
+    # @product_photo = Product_Photo.find(params[:product_photo_id])
     # 商品ごとに複数枚写真を登録できるので、一応全部持ってきておきます。
-    @images = @product.images.all
-
+    
     # まずはログインしているか確認
     if user_signed_in?
+      @user = current_user
       # クレジットカードが登録されているか確認
-      if current_user.credit_card.present?
+      if @user.credit_card.present?
         # 前前前回credentials.yml.encに記載したAPI秘密鍵を呼び出します。
         Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
         # ログインユーザーのクレジットカード情報を引っ張ってきます。
@@ -56,14 +56,9 @@ class PurchasesController < ApplicationController
   def pay
     #ちなみに見やすさ考慮し、before_actionなどのリファクタリングなどはあえてしてません。
     @product = Product.find(params[:product_id])
-    @images = @product.images.all
+    
 
-    # 購入テーブル登録ずみ商品は２重で購入されないようにする
-    # (２重で決済されることを防ぐ)
-    if @product.purchase.present?
-      redirect_to product_path(@product.id), alert: "売り切れています。"
-    else
-      # 同時に2人が同時に購入し、二重で購入処理がされることを防ぐための記述
+    
       @product.with_lock do
         if current_user.credit_card.present?
           # ログインユーザーがクレジットカード登録済みの場合の処理
@@ -79,6 +74,7 @@ class PurchasesController < ApplicationController
           currency: 'jpy'
           )
         else
+          Payjp.api_key = ENV["PAYJP_ACCESS_KEY"]
           # ログインユーザーがクレジットカード登録されていない場合(Checkout機能による処理を行います)
           # APIの「Checkout」ライブラリによる決済処理の記述
           Payjp::Charge.create(
@@ -88,8 +84,12 @@ class PurchasesController < ApplicationController
           )
         end
       #購入テーブルに登録処理(今回の実装では言及しませんが一応、、、)
-      @purchase = Purchase.create(buyer_id: current_user.id, product_id: params[:product_id])
+      @purchase = Purchase.create(buyer_user_id: current_user.id, product_id: params[:product_id])
+      @product.buyer_user_id = current_user.id
+      @product.save
+      
+      
       end
     end
-  end
 end
+
--- db/migrate/20200703111705_create_purchases.rb
+++ db/migrate/20200703111705_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[5.0]
   def change
     create_table :purchases do |t|
-
+      t.integer :buyer_user_id, foreign_key:true
+      t.integer :product_id, foreign_key:true
       t.timestamps
     end
   end
