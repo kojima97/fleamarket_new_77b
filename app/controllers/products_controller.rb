7d09ebc42c4ab44ba58ed4035cdb2070ba60c778
okutsu0519 <65540684+okutsu0519@users.noreply.github.com>
2020-07-07 11:44:58 +0900

    ファイル修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,7 +27,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    # binding.pry
     if @product.save
       redirect_to root_path(@product), notice: '出品完了'
     else
