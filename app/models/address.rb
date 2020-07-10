dced3fa75760e925530d82eb3b449a4e43a94431
kouheimochida <65533949+kouheimochida@users.noreply.github.com>
2020-07-10 12:11:14 +0900

    commit

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,5 +4,6 @@ class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :postal_code, format: { with:/\A[0-9]+\z/ }
   validates :city,:other,:postal_code,:prefecture_id,:first_name,:first_name_furigana,:last_name,:last_name_furigana,  presence: true
-  
+  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name_furigana, :last_name_furigana, format: { with: /\A[ァ-ヶー－]+\z/ }
 end
